c7d2a24192d062244de8adfe5b1bda5bdbedda66
ermilova <dina-er@mail.ru>
2023-01-08 15:26:57 +0300

    piano sounds added

--- Virtual Piano/Virtual Piano/task/src/main.js
+++ Virtual Piano/Virtual Piano/task/src/main.js
@@ -1,4 +1,18 @@
 document.addEventListener("keydown", function(event) {
-    console.log("The '" + event.key + "' key is pressed.");
+    // console.log("The '" + event.key + "' key is pressed.");
+    let key = event.key.toUpperCase();
+    switch (key) {
+        case 'A':
+        case 'D':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'J':
+        case 'S':
+            let audio = new Audio("./key_sounds/" + event.key + ".mp3");
+            audio.play();
+            break;
+
+    }
 
 });
